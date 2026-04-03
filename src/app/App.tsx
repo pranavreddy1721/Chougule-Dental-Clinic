9c15b8421aa48030d659fbe9fb8a77834770a763
Pranav Kaviraj Reddy <pranavreddy1721@gmail.com>
2026-04-03 14:30:44 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import { Navigation } from './components/Navigation';
-import { ThemeToggle } from './components/ThemeToggle';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Services } from './components/Services';
